bda2a43aa2c634e1e4158e37a91bd7e9ffada3a0
will <williamyay95@gmail.com>
2024-10-14 16:11:52 -0400

    b4 exam

--- osed/attach-process-shortcut.ps1
+++ osed/attach-process-shortcut.ps1
@@ -1,6 +1,6 @@
-﻿while ($true) {
+while ($true) {
     try {
-        C:\Users\Offsec\Desktop\setup-uwu\attach-process.ps1 -process-name 'Savant' -commands '.load pykd.pyd'
+        C:\Users\Offsec\Desktop\setup-uwu\attach-process.ps1 -path C:\Windows\System32\notepad.exe -process-name notepad -commands '.load pykd.pyd'
     }
     catch {
         Write-Error "An error occurred: $_"
@@ -11,5 +11,6 @@
 
 
 # .\attach-process.ps1 -service-name 'Disk Pulse Enterprise' -process-name 'diskpls' -commands '.load pykd.pyd; bp 0x101576c0;g;t;t;t'
-#.\attach-process.ps1 -service-name 'Sync Breeze Enterprise' -process-name 'syncbrs' -commands '.load pykd.pyd; bp 0x101576c0;g;t;t;t'
+# .\attach-process.ps1 -service-name 'Sync Breeze Enterprise' -process-name 'syncbrs' -commands '.load pykd.pyd; bp 0x101576c0;g;t;t;t'
 # .\attach-process.ps1 -process-name 'egghhunter_extra_mile' -commands '.load pykd.pyd;bp 0x66601113; g'
+
